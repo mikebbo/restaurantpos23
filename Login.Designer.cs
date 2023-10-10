72213747ce69df4282c1a531b202388665dbaf6e
wisesoft <atmtec@gmail.com>
2023-10-10 17:12:06 -0400

    created the project using vs2023

---
+++ Login.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace RestaurantPOS
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnUserName = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.BtnHelp = new System.Windows.Forms.Button();
+            this.TxtUserName = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnUserName
+            // 
+            this.BtnUserName.Location = new System.Drawing.Point(146, 211);
+            this.BtnUserName.Name = "BtnUserName";
+            this.BtnUserName.Size = new System.Drawing.Size(151, 53);
+            this.BtnUserName.TabIndex = 0;
+            this.BtnUserName.Text = "Ok";
+            this.BtnUserName.UseVisualStyleBackColor = true;
+            this.BtnUserName.Click += new System.EventHandler(this.BtnUserName_Click);
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Location = new System.Drawing.Point(303, 211);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(151, 53);
+            this.BtnCancel.TabIndex = 0;
+            this.BtnCancel.Text = "Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // BtnHelp
+            // 
+            this.BtnHelp.Location = new System.Drawing.Point(460, 211);
+            this.BtnHelp.Name = "BtnHelp";
+            this.BtnHelp.Size = new System.Drawing.Size(151, 53);
+            this.BtnHelp.TabIndex = 0;
+            this.BtnHelp.Text = "Help";
+            this.BtnHelp.UseVisualStyleBackColor = true;
+            this.BtnHelp.Click += new System.EventHandler(this.BtnHelp_Click);
+            // 
+            // TxtUserName
+            // 
+            this.TxtUserName.Location = new System.Drawing.Point(276, 76);
+            this.TxtUserName.Multiline = true;
+            this.TxtUserName.Name = "TxtUserName";
+            this.TxtUserName.Size = new System.Drawing.Size(268, 39);
+            this.TxtUserName.TabIndex = 1;
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Location = new System.Drawing.Point(276, 134);
+            this.TxtPassword.Multiline = true;
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.Size = new System.Drawing.Size(268, 40);
+            this.TxtPassword.TabIndex = 1;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblUserName.Location = new System.Drawing.Point(105, 79);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(127, 31);
+            this.lblUserName.TabIndex = 2;
+            this.lblUserName.Text = "User Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(105, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Password";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(252, 23);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(245, 20);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "Enter Usename to Log in the system";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(677, 324);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.TxtUserName);
+            this.Controls.Add(this.BtnHelp);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnUserName);
+            this.Name = "Login";
+            this.Text = "LOGIN FORM";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button BtnUserName;
+        private Button BtnCancel;
+        private Button BtnHelp;
+        private TextBox TxtUserName;
+        private TextBox TxtPassword;
+        private Label lblUserName;
+        private Label label1;
+        private Label lblTitle;
+
+
+    }
+}
